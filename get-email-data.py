e634bd843414d88d4b8d1a92891796087553308a
Nateliason <nateliason@gmail.com>
2015-01-24 11:22:41 -0500

    Additional comments for clarity

--- get-email-data.py
+++ get-email-data.py
@@ -11,7 +11,7 @@ wks = gc.open("YOUR SPREADSHEET")
 emailSheet = wks.worksheet("Email")
 
 emails = emailSheet.col_values(2) #This will get a list of all the emails in column 2
-emails = emails[1:]
+emails = emails[1:] #This removes the first element of the list (the title of the column)
 
 def getEmailInfo(email):
 	api = TowerDataApi.TowerDataApi(TOWER_DATA_API)
@@ -19,15 +19,15 @@ def getEmailInfo(email):
 	{u'gender': u'Male', u'age': u'25-34'}
 
 for i in range(0,len(emails)):
-	info = getEmailInfo(emails[i])
+	info = getEmailInfo(emails[i]) #This takes every element in "emails" and looks for the gender and age
 	print info
 	   
 	try:
-		emailSheet.update_acell(i+2,4,info["gender"])
+		emailSheet.update_acell(i+2,4,info["gender"]) #This separates the gender element and adds it to your spreadsheet
 	except:
 		emailSheet.update_acell(i+2,4,"none")
 	 
 	try:
-		emailSheet.update_acell(i+2,3,info["age"])
+		emailSheet.update_acell(i+2,3,info["age"]) #This separates the age element and adds it to your spreadsheet
 	except:
 		emailSheet.update_acell(i+2,3,"none")
